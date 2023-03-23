29dd267b996d83644b854414c4dacdd3fd109b3b
chrisaguirre08 <caguirre50307@ufide.ac.cr>
2023-03-22 23:21:49 -0600

    Procedimientos_Almacenados

---
+++ Procedimientos_Almacenados.sql
@@ -0,0 +1,109 @@
+--- Registrar cliente ---
+CREATE OR REPLACE PROCEDURE REGISTRAR_CLIENTE(
+    P_CLIENTE_ID IN Clientes.cliente_id%TYPE,
+    P_NOMBRE_CLIENTE IN Clientes.nombre_cliente%TYPE,
+    P_DIRECCION_CLIENTE IN Clientes.direccion_cliente%TYPE,
+    P_TELEFONO_CLIENTE IN Clientes.telefono_cliente%TYPE,
+    P_CORREO_CLIENTE IN Clientes.correo_cliente%TYPE
+) AS
+BEGIN
+    INSERT INTO Clientes (cliente_id, nombre_cliente, direccion_cliente, telefono_cliente, correo_cliente)
+    VALUES (P_CLIENTE_ID, P_NOMBRE_CLIENTE, P_DIRECCION_CLIENTE, P_TELEFONO_CLIENTE, P_CORREO_CLIENTE);
+    
+    COMMIT;
+END;
+
+BEGIN
+    REGISTRAR_CLIENTE('2', 'John Mesen', 'Calle 123', '6080-9965', 'johnmesen342@ufide.ac.cr');
+END;
+
+--- Modificar Cliente ---
+
+CREATE OR REPLACE PROCEDURE MODIFICAR_CLIENTE(
+    P_CLIENTE_ID IN CLIENTES.CLIENTE_ID%TYPE,
+    P_NOMBRE_CLIENTE IN CLIENTES.NOMBRE_CLIENTE%TYPE,
+    P_DIRECCION_CLIENTE IN CLIENTES.DIRECCION_CLIENTE%TYPE,
+    P_TELEFONO_CLIENTE IN CLIENTES.TELEFONO_CLIENTE%TYPE,
+    P_CORREO_CLIENTE IN CLIENTES.CORREO_CLIENTE%TYPE
+)
+AS
+BEGIN
+    UPDATE CLIENTES
+    SET NOMBRE_CLIENTE = P_NOMBRE_CLIENTE,
+        DIRECCION_CLIENTE = P_DIRECCION_CLIENTE,
+        TELEFONO_CLIENTE = P_TELEFONO_CLIENTE,
+        CORREO_CLIENTE = P_CORREO_CLIENTE
+    WHERE CLIENTE_ID = P_CLIENTE_ID;
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('El cliente con ID ' || P_CLIENTE_ID || ' ha sido modificado exitosamente.');
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Ha ocurrido un error al intentar modificar el cliente con ID ' || P_CLIENTE_ID || '. ' || SQLERRM);
+END;
+
+BEGIN
+    MODIFICAR_CLIENTE('1', 'Carlos Eduardo', 'Calle 123', '6080-9965', 'carloseduardo342@ufide.ac.cr');
+END;
+
+--- Registrar empleado ---
+
+CREATE OR REPLACE PROCEDURE REGISTRAR_EMPLEADO(
+  empleado_id IN Empleados.empleado_id%TYPE,
+  nombre_empleado IN Empleados.nombre_empleado%TYPE,
+  direccion_empleado IN Empleados.direccion_empleado%TYPE,
+  telefono_empleado IN Empleados.telefono_empleado%TYPE,
+  correo_empleado IN Empleados.correo_empleado%TYPE,
+  puesto_empleado IN Empleados.puesto_empleado%TYPE
+) AS
+BEGIN
+  INSERT INTO Empleados(empleado_id, nombre_empleado, direccion_empleado, telefono_empleado, correo_empleado, puesto_empleado)
+  VALUES(empleado_id, nombre_empleado, direccion_empleado, telefono_empleado, correo_empleado, puesto_empleado);
+  COMMIT;
+END;
+
+--- Registrar orden ---
+
+CREATE OR REPLACE PROCEDURE registrar_orden
+(
+  p_orden_id IN Ordenes.orden_id%TYPE,
+  p_cliente_id IN Ordenes.cliente_id%TYPE,
+  p_empleado_id IN Ordenes.empleado_id%TYPE,
+  p_nombre_orden IN Ordenes.nombre_orden%TYPE,
+  p_fecha_orden IN Ordenes.fecha_orden%TYPE,
+  p_descripcion IN Ordenes.descripcion%TYPE
+)
+AS
+BEGIN
+  INSERT INTO Ordenes(orden_id, cliente_id, empleado_id, nombre_orden, fecha_orden, descripcion)
+  VALUES(p_orden_id, p_cliente_id, p_empleado_id, p_nombre_orden, p_fecha_orden, p_descripcion);
+  COMMIT;
+END;
+
+BEGIN
+    registrar_orden('1', '2', '1', 'Orden_Martillo', '10/3/2023', 'Martillos');
+END;
+
+--- Modificar orden ---
+
+CREATE OR REPLACE PROCEDURE modificar_orden (
+    p_orden_id IN ORDENES.ORDEN_ID%TYPE,
+    p_cliente_id IN ORDENES.CLIENTE_ID%TYPE,
+    p_empleado_id IN ORDENES.EMPLEADO_ID%TYPE,
+    p_nombre_orden IN ORDENES.NOMBRE_ORDEN%TYPE,
+    p_fecha_orden IN ORDENES.FECHA_ORDEN%TYPE,
+    p_descripcion IN ORDENES.DESCRIPCION%TYPE
+) AS
+BEGIN
+    UPDATE ORDENES
+    SET 
+        CLIENTE_ID = p_cliente_id,
+        EMPLEADO_ID = p_empleado_id,
+        NOMBRE_ORDEN = p_nombre_orden,
+        FECHA_ORDEN = p_fecha_orden,
+        DESCRIPCION = p_descripcion
+    WHERE ORDEN_ID = p_orden_id;
+    
+    COMMIT;
+END;
+
