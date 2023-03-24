d6a509f725a6dbaf018b7683a0ecf8c73ce4b46d
nachiviris <73266710+nachiviris@users.noreply.github.com>
2023-03-23 18:58:08 -0600

    tablas update
    modificacion de tablas

--- Tablas.sql
+++ Tablas.sql
@@ -19,8 +19,8 @@ CREATE TABLE Empleados (
     empleado_id INT NOT NULL CONSTRAINT PK_EMP_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
     nombre_empleado VARCHAR2(50) NOT NULL,
     direccion_empleado VARCHAR2(100),
-    telefono_cliente VARCHAR2(50),
-    correo_cliente VARCHAR2(40),
+    telefono_empleado VARCHAR2(50),
+    correo_empleado VARCHAR2(40),
     puesto_empleado VARCHAR2(20)
 )TABLESPACE TBS_TS_DATA;
 
@@ -35,4 +35,43 @@ CREATE TABLE Ordenes (
     CONSTRAINT FK_Ordenes_Empleados FOREIGN KEY (empleado_id) REFERENCES Empleados(empleado_id)
 )TABLESPACE TBS_TS_DATA;
 
-select * from Clientes;
+CREATE TABLE DetalleOrdenes (
+    orden_id INT NOT NULL CONSTRAINT PK_DETORD_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
+    producto_id INT NOT NULL,
+    precio_unitario_producto NUMBER (10,2) NOT NULL,
+    cantidad_producto INT NOT NULL,
+    descuento_producto NUMBER (3,2) NOT NULL,
+    descripcion_producto VARCHAR2(100),
+    CONSTRAINT FK_DetalleOrdenes_Ordenes FOREIGN KEY (orden_id) REFERENCES Ordenes(orden_id),
+    CONSTRAINT FK_DetalleOrdenes_Producto FOREIGN KEY (producto_id) REFERENCES Productos(producto_id)
+)TABLESPACE TBS_TS_DATA;
+
+CREATE TABLE Productos (
+    producto_id INT NOT NULL CONSTRAINT PK_PRO_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
+    proveedor_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    nombre_prodcuto VARCHAR2(50) NOT NULL,
+    fecha_orden DATE NOT NULL,
+    descripcion_producto VARCHAR2(100) NOT NULL,
+    CONSTRAINT FK_Productos_Proveedores FOREIGN KEY (proveedor_id) REFERENCES Proveedores(proveedor_id),
+    CONSTRAINT FK_Productos_Categorias FOREIGN KEY (categoria_id) REFERENCES Categorias(producto_id)
+)TABLESPACE TBS_TS_DATA;
+
+CREATE TABLE Proveedores (
+    proveedor_id INT NOT NULL CONSTRAINT PK_PROV_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
+    nombre_proveedor VARCHAR2(50) NOT NULL,
+    nombre_contacto_proveedor VARCHAR2(50) NOT NULL,
+    direccion_proveedor VARCHAR2(50) NOT NULL,
+    telefono_proveedor VARCHAR2(50) NOT NULL
+)TABLESPACE TBS_TS_DATA;
+
+
+CREATE TABLE Categorias (
+    categoria_id INT NOT NULL CONSTRAINT PK_CAT_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
+    nombre_categoria VARCHAR2(50) NOT NULL,
+    descripcion_categoria VARCHAR2(50) NOT NULL
+)TABLESPACE TBS_TS_DATA;
+
+
+
+
