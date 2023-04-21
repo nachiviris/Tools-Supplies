48c5b5d20f53a81a9e5e20920ff9704200f0b1fb
chrisaguirre08 <caguirre50307@ufide.ac.cr>
2023-04-21 00:35:47 -0600

    Updates
    Cambios en BD por un pequeño typo en tabla de Productos y cambios en la conexion de la clase Ctrl_Producto

--- Tools-Supplies/src/controlador/Ctrl_Producto.java
+++ Tools-Supplies/src/controlador/Ctrl_Producto.java
@@ -8,95 +8,97 @@ package controlador;
  *
  * @author crist
  */
+import conexion.Conexion;
 import java.sql.*;
+import java.sql.Connection;
+
 
 public class Ctrl_Producto {
-    private Connection conexion;
 
-    public Ctrl_Producto() {
-        // Realizamos la conexión a la base de datos
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            String servidor = "jdbc:oracle:oci8:@localhost:1521:orcl:Proyecto";
-            String usuario = "";
-            String password = "";
-            conexion = DriverManager.getConnection(servidor, usuario, password);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public void guardarProducto(int producto_id, int proveedor_id, int categoria_id, String nombre_producto, String descripcion_producto, String fecha_orden) {
+    public boolean guardarProducto(int producto_id, int proveedor_id, int categoria_id, String nombre_producto, String descripcion_producto, String fecha_orden) {
         try {
-            String consulta = "INSERT INTO Productos (producto_id, proveedor_id, categoria_id, nombre_producto, descripcion_producto, fecha_orden) VALUES (?, ?, ?, ?, ?, ?)";
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            statement.setInt(1, producto_id);
-            statement.setInt(2, proveedor_id);
-            statement.setInt(3, categoria_id);
-            statement.setString(4, nombre_producto);
-            statement.setString(5, descripcion_producto);
-            statement.setString(6, fecha_orden);
-            statement.executeUpdate();
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Productos (producto_id, proveedor_id, categoria_id, nombre_producto, descripcion_producto, fecha_orden) VALUES (?, ?, ?, ?, ?, ?)");
+            ps.setInt(1, producto_id);
+            ps.setInt(2, proveedor_id);
+            ps.setInt(3, categoria_id);
+            ps.setString(4, nombre_producto);
+            ps.setString(5, descripcion_producto);
+            ps.setString(6, fecha_orden);
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
         } catch (SQLException e) {
-            System.out.println("Error al guardar producto: " + e);
-        }
-    }
-
-    public boolean consultarProducto(int producto_id) {
-        try {
-            String consulta = "SELECT * FROM Productos WHERE producto_id = ?";
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            statement.setInt(1, producto_id);
-            ResultSet resultado = statement.executeQuery();
-            return resultado.next();
-        } catch (SQLException e) {
-            System.out.println("Error al consultar producto: " + e);
+            System.out.println("Error al guardar el producto: " + e.getMessage());
         }
         return false;
     }
 
-    public void actualizarProducto(int producto_id, String nuevoNombre, String nuevaDescripcion, String nuevaFechaOrden) {
+    public boolean productoExiste(int producto_id) {
         try {
-            String consulta = "UPDATE Productos SET nombre_producto = ?, descripcion_producto = ?, fecha_orden = ? WHERE producto_id = ?";
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            statement.setString(1, nuevoNombre);
-            statement.setString(2, nuevaDescripcion);
-            statement.setString(3, nuevaFechaOrden);
-            statement.setInt(4, producto_id);
-            statement.executeUpdate();
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Productos WHERE producto_id = ?");
+            ps.setInt(1, producto_id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
         } catch (SQLException e) {
-            System.out.println("Error al actualizar producto: " + e);
+            System.out.println("Error al buscar el producto: " + e.getMessage());
         }
+        return false;
     }
 
-    public void eliminarProducto(int producto_id) {
+    public boolean actualizarProducto(int producto_id, int proveedor_id, int categoria_id, String nombre_producto, String descripcion_producto, String fecha_orden) {
         try {
-            String consulta = "DELETE FROM Productos WHERE producto_id = ?";
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            statement.setInt(1, producto_id);
-            statement.executeUpdate();
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("UPDATE Productos SET proveedor_id = ?, categoria_id = ?, nombre_producto = ?, descripcion_producto = ?, fecha_orden = ? WHERE producto_id = ?");
+            ps.setInt(1, proveedor_id);
+            ps.setInt(2, categoria_id);
+            ps.setString(3, nombre_producto);
+            ps.setString(4, descripcion_producto);
+            ps.setString(5, fecha_orden);
+            ps.setInt(6, producto_id);
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
         } catch (SQLException e) {
-            System.out.println("Error al eliminar producto: " + e);
+            System.out.println("Error al actualizar el producto: " + e.getMessage());
         }
+        return false;
     }
 
-    public void actualizarStock(int producto_id, int cantidad) {
+    public boolean eliminarProducto(int producto_id) {
         try {
-            String consulta = "UPDATE Productos SET stock = stock + ? WHERE producto_id = ?";
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            statement.setInt(1, cantidad);
-            statement.setInt(2, producto_id);
-            statement.executeUpdate();
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("DELETE FROM Productos WHERE producto_id = ?");
+            ps.setInt(1, producto_id);
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
         } catch (SQLException e) {
-            System.out.println("Error al actualizar stock del producto: " + e);
+            System.out.println("Error al eliminar el producto: " + e.getMessage());
         }
+        return false;
     }
 
-    public void cerrarConexion() {
+    public boolean actualizarStock(int producto_id, int cantidad) {
         try {
-            conexion.close();
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("UPDATE Productos SET stock = stock + ? WHERE producto_id = ?");
+            ps.setInt(1, cantidad);
+            ps.setInt(2, producto_id);
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Error al actualizar el stock del producto: " + e.getMessage());
         }
+        return false;
     }
 }
