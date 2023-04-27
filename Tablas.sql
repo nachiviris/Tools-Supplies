d97e4bcae423a757dda7db817fa94ba2a3e52c7a
nachiviris <73266710+nachiviris@users.noreply.github.com>
2023-04-26 20:49:41 -0600

    tablas modifciones 2

--- Tablas.sql
+++ Tablas.sql
@@ -1,11 +1,8 @@
+alter session set "_ORACLE_SCRIPT" = TRUE;
+
 CREATE TABLESPACE TBS_TS_DATA DATAFILE
 'C:\TBS_TS\TS\DF_TS_DATA.DBF' SIZE 20M DEFAULT STORAGE
 (INITIAL 5M NEXT 5M PCTINCREASE 0);
-
-
-DROP TABLESPACE TBS_TS_DATA INCLUDING CONTENTS;
-DROP TABLESPACE TBS_TS_IDX INCLUDING CONTENTS;
-
 
 CREATE TABLESPACE TBS_TS_IDX DATAFILE
 'C:\TBS_TS\TS\DF_TS_IDX.DBF' SIZE 20M DEFAULT STORAGE
@@ -21,7 +18,6 @@ telefono varchar(15) not null,
 estado int not null
 )TABLESPACE TBS_TS_DATA;
 
-
 create table tb_cliente(
 idCliente int NOT NULL CONSTRAINT PK_CLI_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
 nombre varchar(30) not null,
@@ -29,54 +25,54 @@ apellido varchar(30) not null,
 cedula varchar(15) not null,
 telefono varchar(15) not null,
 direccion varchar(100) not null,
-estado int(1) not null
+estado int not null
 )TABLESPACE TBS_TS_DATA;
-
 
 create table tb_detalle_venta(
-idDetalleVenta int NULL CONSTRAINT PK_DETVEN_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
-idCabeceraVenta int (11) not null,
-idProducto int (11) not null,
-cantidad int(11) not null,
-precioUnitario double(10,2) not null,
-subtotal double(10,2) not null,
-descuento double(10,2) not null,
-iva double(10,2) not null,
-totalPagar double(10,2) not null,
-estado int(1) not null
+idDetalleVenta int NULL,
+idCabeceraVenta int not null,
+idProducto int not null,
+cantidad int not null,
+precioUnitario number(10,2) not null,
+subtotal number(10,2) not null,
+descuento number(10,2) not null,
+iva number(10,2) not null,
+totalPagar number(10,2) not null,
+estado int not null,
+CONSTRAINT PK_DETVEN_ID PRIMARY KEY (idDetalleVenta) USING INDEX TABLESPACE TBS_TS_IDX,
+CONSTRAINT FK_Tbdetalleventa_Cabeceraventa FOREIGN KEY (idCabeceraVenta) REFERENCES tb_cabecera_venta(idCabeceraVenta),
+CONSTRAINT FK_Tbdetalleventa_Producto FOREIGN KEY (idProducto) REFERENCES tb_producto(idProducto)
 )TABLESPACE TBS_TS_DATA;
-
-
 
 create table tb_producto(
-idProducto int NOT NULL CONSTRAINT PK_PRO_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
+idProducto int NOT NULL,
+idCategoria int not null,
 nombre varchar(100) not null,
-cantidad int(11) not null,
-precio double(10,2) not null,
+cantidad int not null,
+precio number(10,2) not null,
 descripcion varchar(200) not null,
-porcentajeIva int(2) not null,
-idCategoria int(11) not null,
-estado int(1) not null
+porcentajeIva int not null,
+estado int not null,
+CONSTRAINT PK_PRO_ID PRIMARY KEY (idProducto) USING INDEX TABLESPACE TBS_TS_IDX,
+CONSTRAINT FK_Tbproducto_Categoria FOREIGN KEY (idCategoria) REFERENCES tb_categoria(idCategoria)
 )TABLESPACE TBS_TS_DATA;
+
 
 create table tb_cabecera_venta(
-idCabeceraVenta int NOT NULL CONSTRAINT PK_CV_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
-idCliente int(11) not null,
-valorPagar double(10,2) not null,
+idCabeceraVenta int NOT NULL,
+idCliente int not null,
+valorPagar number(10,2) not null,
 fechaVenta date not null,
-estado int(1) not null
+estado int not null,
+    CONSTRAINT PK_CV_ID PRIMARY KEY (idCabeceraVenta) USING INDEX TABLESPACE TBS_TS_IDX,
+    CONSTRAINT FK_CabeceraVenta_Cliente FOREIGN KEY (idCliente) REFERENCES tb_cliente(idCliente)
 )TABLESPACE TBS_TS_DATA;
-
 
 
 
 create table tb_categoria(
 idCategoria int NOT NULL CONSTRAINT PK_CAT_ID PRIMARY KEY USING INDEX TABLESPACE TBS_TS_IDX,
 descripcion varchar(200) not null,
-estado int(1) not null
+estado int not null
 )TABLESPACE TBS_TS_DATA;
-
-
-
-
 
